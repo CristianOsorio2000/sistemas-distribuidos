9eb6d23f9f4519dc10eebe410dcdf3df3195bb3b
cristian <cristianandres3012@gmail.com>
2024-11-02 15:15:58 -0500

    Trabajo clase 2 de noviembre 2024

--- db-instances/contenedor-postgres-ppal.sh
+++ db-instances/contenedor-postgres-ppal.sh
@@ -1,1 +1,5 @@
-docker run --name postgres-ppal --network challenge-postgres -e POSTGRES_PASSWORD=postgres -p 5432:5432 -d postgres
+docker run --name postgres-ppal --network challenge-postgres\
+    -e POSTGRES_HOST_AUTH_METHOD=md5 \
+    -e POSTGRES_PASSWORD=postgres \
+    -p 5432:5432 -d postgres
+    -c 'config_file=/etc/postgresql/postgresql.conf'
